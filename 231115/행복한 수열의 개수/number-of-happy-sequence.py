bdc2a4dd4407408a37806525fba42fcf63b676ad
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-11-15 09:24:32 +0900

    Title: 행복한 수열의 개수, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/행복한 수열의 개수/number-of-happy-sequence.py
+++ 231115/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,30 +4,28 @@ for n in range(N) :
     line = list(map(int,input().split()))
     board.append(line)
 answer = 0
-for i in range(N) :
-    temp = []
-    for j in range(N-1) :
-        if board[i][j] == board[i][j+1] :
-            temp.append(board[i][j])
-            temp.append(board[i][j+1])
-            if len(temp) == M :
-                answer += 1
-                break
-for i in range(N) :
-    temp = []
-    for j in range(N-1) :
-        if board[j][i] == board[j+1][i] :
-            temp.append(board[i][j])
-            temp.append(board[i][j+1])
-            if len(temp) == M :
-                answer += 1
-                break
-arr = []
 if M == 1 :
     answer = 2*N
-    # for i in range(N) :
-    #     for j in range(N) :
-    #         arr.append(board[i][j])
-    # arr = list(set(arr))
-    # answer = len(arr)
+else :
+    for i in range(N) :
+        temp = []
+        for j in range(N-1) :
+            if board[i][j] == board[i][j+1] :
+                # print(i,j,board[i][j],board[i][j+1])
+                temp.append(board[i][j])
+                # temp.append(board[i][j+1])
+                if len(temp) == M :
+                    # print(temp)
+                    answer += 1
+                    # print(answer)
+                    break
+    for i in range(N) :
+        temp = []
+        for j in range(N-1) :
+            if board[j][i] == board[j+1][i] :
+                temp.append(board[i][j])
+                # temp.append(board[i][j+1])
+                if len(temp) == M :
+                    answer += 1
+                    break
 print(answer)
