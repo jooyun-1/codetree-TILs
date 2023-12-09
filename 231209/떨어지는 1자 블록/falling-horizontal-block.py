e68ee93cfb9f38bdef65fe878bb4ed8e187e185b
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-12-09 21:17:01 +0900

    Title: 떨어지는 1자 블록, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231209/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,35 @@
+n, m, k = map(int,input().split())
+board = []
+for i in range(n) :
+    line = list(map(int,input().split()))
+    board.append(line)
+k -= 1
+
+dx = [1,0]
+
+def check(x,y) :
+    nx = x + 1
+    ny = y
+    flag = False
+    if 0 <= nx < n and 0 <= ny < n :
+        if board[nx][ny] == 1 :
+            flag = True
+            return flag
+        else :
+            return flag
+    return flag
+
+for i in range(n) :
+    total_flag = False
+    for j in range(m) :
+        result = check(i,j)
+        if result :
+            total_flag = True
+            board[i][:m] = [1] * m
+            break
+    if total_flag :
+        break
+for i in range(len(board)) :
+    for j in range(len(board[i])) :
+        print(board[i][j], end=' ')
+    print()
