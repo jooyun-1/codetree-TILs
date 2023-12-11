e4fb1b0e8841bb04b13ece5fe1884b45245bc232
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-12-11 14:48:23 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231211/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 231211/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -7,27 +7,37 @@
 def escape(x,y,cur_dir) :
     global time
 
+    if visited[x][y] == 0 :
+        visited[x][y] = 1
+    else :
+        time = -1
+        return
     dirs = [[0,1],[-1,0],[0,-1],[1,0]]
     clock_dirs = [[0,1],[1,0],[0,-1],[1,0]]
     nx = x + dirs[cur_dir][0]
     ny = y + dirs[cur_dir][1]
+
     visited.append([x,y])
-    if [nx,ny] in visited :
-        time = -1
+
     if 0 <= nx < N and 0 <= ny < N :
         if miro[nx][ny] == '#' :
             cur_dir = (cur_dir+1) % 4
             escape(x,y,cur_dir)
         else :
-            right_dir = (cur_dir+1) % 4
-            rx = nx + clock_dirs[right_dir][0]
-            ry = ny + clock_dirs[right_dir][1]
+            right_dir = cur_dir - 1
+            if right_dir < 0 :
+                right_dir = 3
+            # right_dir = (cur_dir+1) % 4
+            # rx = nx + clock_dirs[right_dir][0]
+            # ry = ny + clock_dirs[right_dir][1]
+            rx = nx + dirs[right_dir][0]
+            ry = ny + dirs[right_dir][1]
             if 0 <= rx < N and 0 <= ry < N :
                 if miro[rx][ry] == '#' :
                     time += 1
                     escape(nx,ny,cur_dir)
                 else :
-                    time += 1
+                    time += 2
                     escape(rx,ry,right_dir)
     else :
         time += 1
@@ -37,7 +47,7 @@ x, y = map(int,input().split())
 x -= 1
 y -= 1
 miro = []
-visited = []
+visited = [[0] * N for _ in range(N)]
 for n in range(N) :
     line = input().rstrip()
     temp = []
