276c38c3d5f2a5f7422a4209bfb09cee95ddae8a
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-05-11 20:20:26 +0900

    Title: 최고의 33위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250511/최고의 33위치/best-place-of-33.py
+++ 250511/최고의 33위치/best-place-of-33.py
@@ -2,11 +2,17 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 answer = 0
 
-for i in range(n-2) :
+def get_coins(r,r2,c,c2) :
     cnt = 0
-    for a in range(i,i+3) :
-        for b in range(i,i+3) :
-            if grid[a][b] == 1 :
+    for i in range(r, r2+1) :
+        for j in range(c, c2+1) :
+            if grid[i][j] == 1 :
                 cnt += 1
-    answer = max(answer,cnt)
+    return cnt
+
+for i in range(n) :
+    for j in range(n) :
+        if i + 2 >= n or j + 2 >= n :
+            continue
+        answer = max(answer,get_coins(i,i+2,j,j+2))    
 print(answer)
